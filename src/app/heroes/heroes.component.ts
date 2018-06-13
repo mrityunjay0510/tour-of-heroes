f15f469ebbbb41cf2599a921b46bc99ac26f0420
mrityunjay.singh <mrityunjay.singh@kdi.kongsberg.com>
2018-06-13 20:31:35 +0530

    Add Dashbaord

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
-<<<<<<< HEAD
-import {HeroService} from '../hero.service';
-=======
 import { HeroService } from '../hero.service';
->>>>>>> 3990739fb95f4148bbb32027cb7954f22d8830c2
 
 @Component({
   selector: 'app-heroes',
@@ -13,22 +9,10 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-<<<<<<< HEAD
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-
-  constructor(private heroService:HeroService) {
-  }
-
-  // heroes = HEROES;
-=======
   constructor(private heroService: HeroService) {
   }
 
   //heroes = HEROES;
->>>>>>> 3990739fb95f4148bbb32027cb7954f22d8830c2
   heroes:Hero[];
   selectedHero: Hero;
 
@@ -44,8 +28,8 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes =>this.heroes = heroes);
   }
 
-  getHeroes():void{
-    this.heroes=this.heroService.getHeroes();
-  } 
+  // getHeroes():void{
+  //   this.heroes=this.heroService.getHeroes();
+  // } 
 
 }
